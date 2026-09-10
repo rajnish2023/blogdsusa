eea5570b5725d2a4f6105a366cc09a8de417a653
rajnish2023 <rajnish.kumar@dynamicssquare.com>
2026-09-10 17:32:59 +0530

    refactor database connection in migrateSlugs.js to use environment variable for MongoDB URI

--- backend/scripts/migrateSlugs.js
+++ backend/scripts/migrateSlugs.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const User = require("../models/User"); // Path assuming script is in backend/scripts
 
-mongoose.connect(process.env.MONGO_URI || "mongodb+srv://danish0007865:Q1n0oXF6r2y7aFmC@ds2025.w9nmy.mongodb.net/blogds?retryWrites=true&w=majority&appName=DS2025").then(async () => {
+mongoose.connect(process.env.MONGO_URI).then(async () => {
   const users = await User.find({});
   for (const user of users) {
     if (!user.authorSlug) {
