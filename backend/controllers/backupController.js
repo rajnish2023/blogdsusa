7e4053b7ca522deb7b4032c789e4cd401d6e577f
rajnish2023 <rajnish.kumar@dynamicssquare.com>
2026-09-14 16:39:13 +0530

    enhance backup functionality; include additional models in backup and import processes, and increase file size limit to 100MB

--- backend/controllers/backupController.js
+++ backend/controllers/backupController.js
@@ -10,6 +10,19 @@ const Media = require("../models/Media");
 const Page = require("../models/Page");
 const PageCategory = require("../models/PageCategory");
 const Blog = require("../models/Blog");
+const Currency = require("../models/Currency");
+const Estimator = require("../models/Estimator");
+const EstimatorQuestion = require("../models/EstimatorQuestion");
+const EstimatorResponse = require("../models/EstimatorResponse");
+const EstimatorResult = require("../models/EstimatorResult");
+const EstimatorService = require("../models/EstimatorService");
+const Lead = require("../models/Lead");
+const LicensingCapability = require("../models/LicensingCapability");
+const LicensingContent = require("../models/LicensingContent");
+const LicensingGroup = require("../models/LicensingGroup");
+const LicensingLead = require("../models/LicensingLead");
+const LicensingPricing = require("../models/LicensingPricing");
+const Setting = require("../models/Setting");
 
 const os = require("os");
 
@@ -26,7 +39,7 @@ const backupUpload = multer({
       cb(new Error("Only JSON backup files are allowed"), false);
     }
   },
-  limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
+  limits: { fileSize: 100 * 1024 * 1024 }, // increased to 100MB
 });
 
 exports.backupUploadMiddleware = backupUpload.single("backupFile");
@@ -34,7 +47,12 @@ exports.backupUploadMiddleware = backupUpload.single("backupFile");
 exports.exportBackup = async (req, res) => {
   try {
    
-    const [roles, users, categories, media, pages, pageCategories, blogs] = await Promise.all([
+    const [
+      roles, users, categories, media, pages, pageCategories, blogs,
+      currencies, estimators, estimatorQuestions, estimatorResponses, estimatorResults,
+      estimatorServices, leads, licensingCapabilities, licensingContents, licensingGroups,
+      licensingLeads, licensingPricings, settings
+    ] = await Promise.all([
       Role.find({}),
       User.find({}).select("+password +failedLoginAttempts +lockUntil +refreshTokenHash +passwordResetToken +passwordResetExpires"),
       Category.find({}),
@@ -42,22 +60,32 @@ exports.exportBackup = async (req, res) => {
       Page.find({}),
       PageCategory.find({}),
       Blog.find({}),
+      Currency.find({}),
+      Estimator.find({}),
+      EstimatorQuestion.find({}),
+      EstimatorResponse.find({}),
+      EstimatorResult.find({}),
+      EstimatorService.find({}),
+      Lead.find({}),
+      LicensingCapability.find({}),
+      LicensingContent.find({}),
+      LicensingGroup.find({}),
+      LicensingLead.find({}),
+      LicensingPricing.find({}),
+      Setting.find({})
     ]);
 
     const backupData = {
       metadata: {
-        version: "1.0",
+        version: "1.1",
         exportedAt: new Date().toISOString(),
         database: mongoose.connection.name,
       },
       collections: {
-        roles,
-        users,
-        categories,
-        media,
-        pages,
-        pageCategories,
-        blogs,
+        roles, users, categories, media, pages, pageCategories, blogs,
+        currencies, estimators, estimatorQuestions, estimatorResponses, estimatorResults,
+        estimatorServices, leads, licensingCapabilities, licensingContents, licensingGroups,
+        licensingLeads, licensingPricings, settings
       },
     };
 
@@ -90,7 +118,12 @@ exports.importBackup = async (req, res) => {
       return res.status(400).json({ message: "Invalid backup file structure. Metadata or collections missing." });
     }
 
-    const { roles, users, categories, media, pages, pageCategories, blogs } = backup.collections;
+    const {
+      roles, users, categories, media, pages, pageCategories, blogs,
+      currencies, estimators, estimatorQuestions, estimatorResponses, estimatorResults,
+      estimatorServices, leads, licensingCapabilities, licensingContents, licensingGroups,
+      licensingLeads, licensingPricings, settings
+    } = backup.collections;
  
     const totalRecords =
       (roles || []).length +
@@ -99,7 +132,10 @@ exports.importBackup = async (req, res) => {
       (media || []).length +
       (pages || []).length +
       (pageCategories || []).length +
-      (blogs || []).length;
+      (blogs || []).length +
+      (currencies || []).length +
+      (estimators || []).length +
+      (licensingCapabilities || []).length;
 
     if (totalRecords === 0) {
       return res.status(400).json({
@@ -115,11 +151,38 @@ exports.importBackup = async (req, res) => {
       Page.deleteMany({}),
       PageCategory.deleteMany({}),
       Blog.deleteMany({}),
+      Currency.deleteMany({}),
+      Estimator.deleteMany({}),
+      EstimatorQuestion.deleteMany({}),
+      EstimatorResponse.deleteMany({}),
+      EstimatorResult.deleteMany({}),
+      EstimatorService.deleteMany({}),
+      Lead.deleteMany({}),
+      LicensingCapability.deleteMany({}),
+      LicensingContent.deleteMany({}),
+      LicensingGroup.deleteMany({}),
+      LicensingLead.deleteMany({}),
+      LicensingPricing.deleteMany({}),
+      Setting.deleteMany({})
     ]);
  
-    if (Array.isArray(roles) && roles.length) {
-      await Role.collection.insertMany(roles.map(r => ({ ...r, _id: new mongoose.Types.ObjectId(r._id) })));
-    }
+    if (Array.isArray(roles) && roles.length) await Role.collection.insertMany(roles.map(r => ({ ...r, _id: new mongoose.Types.ObjectId(r._id) })));
+    if (Array.isArray(categories) && categories.length) await Category.collection.insertMany(categories.map(c => ({ ...c, _id: new mongoose.Types.ObjectId(c._id) })));
+    if (Array.isArray(media) && media.length) await Media.collection.insertMany(media.map(m => ({ ...m, _id: new mongoose.Types.ObjectId(m._id) })));
+    if (Array.isArray(pageCategories) && pageCategories.length) await PageCategory.collection.insertMany(pageCategories.map(pc => ({ ...pc, _id: new mongoose.Types.ObjectId(pc._id) })));
+    if (Array.isArray(currencies) && currencies.length) await Currency.collection.insertMany(currencies.map(c => ({ ...c, _id: new mongoose.Types.ObjectId(c._id) })));
+    if (Array.isArray(estimators) && estimators.length) await Estimator.collection.insertMany(estimators.map(e => ({ ...e, _id: new mongoose.Types.ObjectId(e._id) })));
+    if (Array.isArray(estimatorQuestions) && estimatorQuestions.length) await EstimatorQuestion.collection.insertMany(estimatorQuestions.map(e => ({ ...e, _id: new mongoose.Types.ObjectId(e._id) })));
+    if (Array.isArray(estimatorResponses) && estimatorResponses.length) await EstimatorResponse.collection.insertMany(estimatorResponses.map(e => ({ ...e, _id: new mongoose.Types.ObjectId(e._id) })));
+    if (Array.isArray(estimatorResults) && estimatorResults.length) await EstimatorResult.collection.insertMany(estimatorResults.map(e => ({ ...e, _id: new mongoose.Types.ObjectId(e._id) })));
+    if (Array.isArray(estimatorServices) && estimatorServices.length) await EstimatorService.collection.insertMany(estimatorServices.map(e => ({ ...e, _id: new mongoose.Types.ObjectId(e._id) })));
+    if (Array.isArray(leads) && leads.length) await Lead.collection.insertMany(leads.map(l => ({ ...l, _id: new mongoose.Types.ObjectId(l._id) })));
+    if (Array.isArray(licensingCapabilities) && licensingCapabilities.length) await LicensingCapability.collection.insertMany(licensingCapabilities.map(l => ({ ...l, _id: new mongoose.Types.ObjectId(l._id) })));
+    if (Array.isArray(licensingContents) && licensingContents.length) await LicensingContent.collection.insertMany(licensingContents.map(l => ({ ...l, _id: new mongoose.Types.ObjectId(l._id) })));
+    if (Array.isArray(licensingGroups) && licensingGroups.length) await LicensingGroup.collection.insertMany(licensingGroups.map(l => ({ ...l, _id: new mongoose.Types.ObjectId(l._id) })));
+    if (Array.isArray(licensingLeads) && licensingLeads.length) await LicensingLead.collection.insertMany(licensingLeads.map(l => ({ ...l, _id: new mongoose.Types.ObjectId(l._id) })));
+    if (Array.isArray(licensingPricings) && licensingPricings.length) await LicensingPricing.collection.insertMany(licensingPricings.map(l => ({ ...l, _id: new mongoose.Types.ObjectId(l._id) })));
+    if (Array.isArray(settings) && settings.length) await Setting.collection.insertMany(settings.map(s => ({ ...s, _id: new mongoose.Types.ObjectId(s._id) })));
  
     if (Array.isArray(users) && users.length) {
       await User.collection.insertMany(users.map(u => ({
@@ -127,18 +190,6 @@ exports.importBackup = async (req, res) => {
         _id: new mongoose.Types.ObjectId(u._id),
         role: u.role ? new mongoose.Types.ObjectId(u.role) : null,
       })));
-    }
- 
-    if (Array.isArray(categories) && categories.length) {
-      await Category.collection.insertMany(categories.map(c => ({ ...c, _id: new mongoose.Types.ObjectId(c._id) })));
-    }
- 
-    if (Array.isArray(media) && media.length) {
-      await Media.collection.insertMany(media.map(m => ({ ...m, _id: new mongoose.Types.ObjectId(m._id) })));
-    }
- 
-    if (Array.isArray(pageCategories) && pageCategories.length) {
-      await PageCategory.collection.insertMany(pageCategories.map(pc => ({ ...pc, _id: new mongoose.Types.ObjectId(pc._id) })));
     }
  
     if (Array.isArray(pages) && pages.length) {
@@ -170,6 +221,8 @@ exports.importBackup = async (req, res) => {
         media: (media || []).length,
         pages: (pages || []).length,
         blogs: (blogs || []).length,
+        estimators: (estimators || []).length,
+        licensing: (licensingCapabilities || []).length,
       },
     });
   } catch (err) {
